41192ce187bdcba01328270cf3d3b3e3e6937cd5
Obi Nkemjika Derick <obinkemjikaderick@Obis-MacBook-Pro.local>
2022-08-12 12:52:51 +0100

    send score

--- components/Wordle/Wordle.tsx
+++ components/Wordle/Wordle.tsx
@@ -12,44 +12,66 @@ import { ImSpinner9 } from 'react-icons/im';
 const Wordle = () => {
 	const [isSendingScore, setIsSendingScore] = useState(false);
 	const router = useRouter();
-    const { gameOver, generateWords, currentAttempt } = useBoard();
-	const { currentTournament, players, address, sendScore, wordChainContract } = useWallet();
+	const { gameOver, generateWords, currentAttempt } = useBoard();
+	const { currentTournament, players, address, sendScore, wordChainContract } =
+		useWallet();
 
-    const handleGameOver = async () => {
-		let playerTournamentId = players.filter((t:any) => t.address === address)[0].id;
+	const handleGameOver = async () => {
+		let playerTournamentId = players.filter(
+			(t: any) => t.address === address
+		)[0].id;
+		console.log({ playerTournamentId });
 
 		try {
 			setIsSendingScore(true);
-			await sendScore(wordChainContract, address, currentTournament, 7 - currentAttempt.attempt, playerTournamentId);
-			showToast("Score saved", NotificationType.SUCCESS);
+			if (gameOver.guessedWord) {
+				await sendScore(
+					wordChainContract,
+					address,
+					currentTournament,
+					7 - currentAttempt.attempt,
+					playerTournamentId
+				);
+			} else {
+				await sendScore(
+					wordChainContract,
+					address,
+					currentTournament,
+					0,
+					playerTournamentId
+				);
+			}
+
+			showToast('Score saved', NotificationType.SUCCESS);
 		} catch (error) {
 			showToast((error as Error).message, NotificationType.ERROR);
 		}
 		setIsSendingScore(false);
-    }
-    
+	};
 
 	useEffect(() => {
 		if (wordChainContract === null) {
-			router.push("/");
+			router.push('/');
 		}
 		generateWords();
 		//eslint-disable-next-line
 	}, []);
 
 	useEffect(() => {
-		if(gameOver.gameOver) {
+		if (gameOver.gameOver) {
 			handleGameOver();
 		}
 		//eslint-disable-next-line
 	}, [gameOver.gameOver]);
 	return (
-        <div className='w-[800px] mx-auto'>
-			{isSendingScore && <div className= 'absolute top-4 right-4 h-[50px] w-48 rounded-lg bg-[aliceblue] flex items-center justify-center'>
-				<ImSpinner9 className='animate-spin h-5 w-5 mr-3' />
-				<p>Sending Score...</p>
-			</div>}
-            <h1 className='text-7xl text-center text-white pt-12 mb-4'>Wordle</h1>
+		<div className='w-[800px] mx-auto'>
+			{isSendingScore && (
+				<div className='absolute top-4 right-4 h-[50px] w-48 rounded-lg bg-[aliceblue] flex items-center justify-center'>
+					<ImSpinner9 className='animate-spin h-5 w-5 mr-3' />
+					<p>Sending Score...</p>
+				</div>
+			)}
+			<h1 className='text-7xl text-center text-white pt-12 mb-4'>Wordle</h1>
 			<Board />
 			{gameOver.gameOver ? <GameOver /> : <Keyboard />}
 		</div>
--- pages/dashboard/tournaments/[id]/index.tsx
+++ pages/dashboard/tournaments/[id]/index.tsx
@@ -8,15 +8,21 @@ import Moment from 'react-moment';
 
 const SingleTournamentPage = ({ id }: any) => {
 	const [tournament_, setTournament_] = useState({
-		name: "", 
-		description: "", 
-		totalStake: 0, 
+		name: '',
+		description: '',
+		totalStake: 0,
 		deadline: 3601,
 		minimumStakeAmount: 0,
 	});
 	const [loading, setLoading] = useState(true);
-	const { fetchAllPlayers, fetchAllTournaments, wordChainContract, tournaments, players } =
-		useWallet();
+	const {
+		fetchAllPlayers,
+		setCurrentTournament,
+		fetchAllTournaments,
+		wordChainContract,
+		tournaments,
+		players,
+	} = useWallet();
 
 	useEffect(() => {
 		if (wordChainContract !== null) {
@@ -32,70 +38,89 @@ const SingleTournamentPage = ({ id }: any) => {
 		}
 		//eslint-disable-next-line
 	}, [tournaments]);
+
+	useEffect(() => {
+		if (id) {
+			setCurrentTournament(id);
+		}
+		//eslint-disable-next-line
+	}, [id]);
 	const router = useRouter();
 	return (
 		<BasePageLayout>
 			<div className='w-[900px] mx-auto'>
-				{!loading ? <><div className='mt-16 mr-4 pl-10'>
-					<h3 className='text-7xl text-center mb-8 drop-shadow-sm'>
-						{tournament_.name}
-					</h3>
-					<p className='text-center'>
-						{tournament_.name}
-					</p>
-					<p className='text-center'>
-						Number of Participants: <span>{players && players.length}</span>
-					</p>
-					<p className='text-center'>Total Stake: <span className= 'font-bold'>{tournament_.totalStake} WCT</span></p>
-					
-				</div>
-				<div className='flex items-center justify-between text-[0.8rem]'>
-					<p>Minimum Stake: {tournament_.minimumStakeAmount} WCT</p>
-					<p>Deadline: <Moment unix format='YYYY-MM-DD HH:mm UTC'>
-						{Number(tournament_.deadline) - 3600}
-					</Moment>
-					</p>
-				</div>
-				{players ? (
-					<table className='table-auto mt-8 w-full '>
-						<thead>
-							<tr className='border-b-2 text-4xl'>
-								<th className='w-[10%]'>S/N</th>
-								<th className='w-1/2'>Username</th>
-								<th className='w-1/5'>Score</th>
-								<th className='w-1/5 whitespace-nowrap'>Games Played</th>
-							</tr>
-						</thead>
+				{!loading ? (
+					<>
+						<div className='mt-16 mr-4 pl-10'>
+							<h3 className='text-7xl text-center mb-8 drop-shadow-sm'>
+								{tournament_.name}
+							</h3>
+							<p className='text-center'>{tournament_.name}</p>
+							<p className='text-center'>
+								Number of Participants: <span>{players && players.length}</span>
+							</p>
+							<p className='text-center'>
+								Total Stake:{' '}
+								<span className='font-bold'>{tournament_.totalStake} WCT</span>
+							</p>
+						</div>
+						<div className='flex items-center justify-between text-[0.8rem]'>
+							<p>Minimum Stake: {tournament_.minimumStakeAmount} WCT</p>
+							<p>
+								Deadline:{' '}
+								<Moment unix format='YYYY-MM-DD HH:mm UTC'>
+									{Number(tournament_.deadline) - 3600}
+								</Moment>
+							</p>
+						</div>
+						{players ? (
+							<table className='table-auto mt-8 w-full '>
+								<thead>
+									<tr className='border-b-2 text-4xl'>
+										<th className='w-[10%]'>S/N</th>
+										<th className='w-1/2'>Username</th>
+										<th className='w-1/5'>Score</th>
+										<th className='w-1/5 whitespace-nowrap'>Games Played</th>
+									</tr>
+								</thead>
 
-						<tbody className='text-xl '>
-							{players &&
-								players
-								.sort((a: any, b: any) => b.gamesPlayed - a.gamesPlayed)
-								.sort((a: any, b: any) => b.score - a.score)
-									.map((player: any, index: any) => (
-										<tr
-											key={index}
-											className='border-b-2 h-16 hover:bg-gray-200 text-gray-600 text-center'
-										>
-											<td className='text-[#0E1027]'>{index + 1}</td>
-											<td className='text-[#0E1027]'>{player.username}</td>
-											<td className='text-[#0E1027]'>{player.score.toFixed(2)}</td>
-											<td className='text-[#0E1027]'>{player.gamesPlayed}</td>
-										</tr>
-									))}
-						</tbody>
-					</table>
+								<tbody className='text-xl '>
+									{players &&
+										players
+											.sort((a: any, b: any) => b.gamesPlayed - a.gamesPlayed)
+											.sort((a: any, b: any) => b.score - a.score)
+											.map((player: any, index: any) => (
+												<tr
+													key={index}
+													className='border-b-2 h-16 hover:bg-gray-200 text-gray-600 text-center'
+												>
+													<td className='text-[#0E1027]'>{index + 1}</td>
+													<td className='text-[#0E1027]'>{player.username}</td>
+													<td className='text-[#0E1027]'>
+														{player.score.toFixed(2)}
+													</td>
+													<td className='text-[#0E1027]'>
+														{player.gamesPlayed}
+													</td>
+												</tr>
+											))}
+								</tbody>
+							</table>
+						) : (
+							<div>There are currently no players...</div>
+						)}
+						<button
+							onClick={() => {
+								router.push('/dashboard/game/play');
+							}}
+							className='border mt-8 border-[#0E1027] p-3 bg-[#0E1027] text-white w-32 rounded-md uppercase'
+						>
+							start game
+						</button>
+					</>
 				) : (
-					<div>There are currently no players...</div>
+					<ImSpinner9 className='animate-spin h-16 w-16 mr-auto ml-auto mt-48' />
 				)}
-				<button
-					onClick={() => {
-						router.push('/dashboard/game/play');
-					}}
-					className='border mt-8 border-[#0E1027] p-3 bg-[#0E1027] text-white w-32 rounded-md uppercase'
-				>
-					start game
-				</button></> : <ImSpinner9 className='animate-spin h-16 w-16 mr-auto ml-auto mt-48' />}
 			</div>
 		</BasePageLayout>
 	);
